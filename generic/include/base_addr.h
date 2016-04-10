d0eab443b03facc141235b9ca108c5c135a7c681
dhytxz <hding4@gmail.com>
2016-04-10 16:16:07 -0500

    update base address

--- generic/include/base_addr.h
+++ generic/include/base_addr.h
@@ -5,8 +5,8 @@
 #define ARM_HTDT_BASE                       0x40000000
 #define ARM_DDR_BASE                        0x60000000
 
-#define DMA_LOCAL_MEM_BASE                  0xD0008000
-#define DMA_INST_MEM_BASE                   0xD0008000
+#define DMA_LOCAL_MEM_BASE                  0xC0000000
+#define DMA_INST_MEM_BASE                   0xC2000000
 #define DMA_DDR_BASE                        0x60000000
 
 #define SCHEDULER_DMA_BASE                  0x44A10000
@@ -16,7 +16,7 @@
 #define SCHEDULER_ID_GENERATOR_BASE         0x44A00000
 
 #define SLAVE_LOCAL_MEM_BASE                0xC0000000
-#define SLAVE_INST_MEM_BASE                 0xC0000000
+#define SLAVE_INST_MEM_BASE                 0x00008000
 #define SLAVE_DDR_BASE                      0x60000000
 
 #endif
