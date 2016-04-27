e640cc64719fcd3665f577a0b21c6c2ac696bc2c
dhytxz <hding4@gmail.com>
2016-04-27 16:30:15 -0500

    add hapara_burst_icap ip

---
+++ ip_repo/hapara_burst_icap/hapara_burst_icap.v
@@ -0,0 +1,35 @@
+
+`timescale 1 ns / 1 ps
+
+module hapara_burst_icap #
+(
+	parameter integer DATA_WIDTH = 32
+)
+(
+	input  clk,
+	input  rst,
+	input  en,
+	input  [DATA_WIDTH / 8 - 1 : 0] we,
+	input  [DATA_WIDTH - 1 : 0] addr,
+	input  [DATA_WIDTH - 1 : 0] din,
+	output  [DATA_WIDTH - 1 : 0] dout
+);
+
+wire ready = &we;
+
+ICAPE2 #
+(
+	.DEVICE_ID(0'h3651093),
+	.ICAP_WIDTH("X32"),
+	.SIM_CFG_FILE_NAME("None")
+)
+ICAPE2_inst (
+	.O(),
+	.CLK(clk),
+	.CSIB(ready),
+	.I(din),
+	.RDWRB(1'b0)
+);
+
+
+endmodule
