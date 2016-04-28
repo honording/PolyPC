e063556d2132be1a954870c1495d4a4aec238a3d
dhytxz <hding4@gmail.com>
2016-04-28 16:45:05 -0500

    fix hapara_burst_icap bugs

--- ip_repo/hapara_burst_icap/hapara_burst_icap.v
+++ ip_repo/hapara_burst_icap/hapara_burst_icap.v
@@ -22,7 +22,7 @@ wire [DATA_WIDTH - 1 : 0] swap_little;
 localparam NUM_OF_BYTE = DATA_WIDTH / 8;
 generate
 	genvar i;
-	for (i = 0; i < NUM_OF_BYTE; i++) begin
+	for (i = 0; i < NUM_OF_BYTE; i = i + 1) begin
 		hapara_burst_icap_bitswap bitswap_bit (
 			.din(din[8 * (i + 1) - 1: 8 * i]),
 			.dout(swap_little[8 * (4 - i) - 1: 8 * (3 - i)])
--- ip_repo/hapara_burst_icap/hapara_burst_icap_bitswap.v
+++ ip_repo/hapara_burst_icap/hapara_burst_icap_bitswap.v
@@ -1,6 +1,6 @@
 
 `timescale 1 ns / 1 ps
-module hapara_burst_icap_bitswap (
+module hapara_burst_icap_bitswap # (
     parameter integer DATA_WIDTH = 8
 )
 (
