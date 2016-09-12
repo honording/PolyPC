01e7a5bd32d7d0412df9b3dfeccc75cae61a829c
Hongyuan Ding <hding4@gmail.com>
2016-09-11 22:03:38 -0500

    update apploadpr

--- software/petalinux/apps/apploadpr/apploadpr.c
+++ software/petalinux/apps/apploadpr/apploadpr.c
@@ -35,17 +35,17 @@
 // #define PR_SIZE         580532
 #define PR_SIZE         __vector_add_pr_add_bin_len
 
-#define rp_math_STATUS        0X00000
-#define rp_math_CONTROL       0X00000
-#define rp_math_SW_TRIGGER    0X00004
-#define rp_math_TRIGGER0      0X00020
+#define vsm_off | rm_math_STATUS        0X00000
+#define vsm_off | rm_math_CONTROL       0X00000
+#define vsm_off | rm_math_SW_TRIGGER    0X00004
+#define vsm_off | rm_math_TRIGGER0      0X00020
 
-#define rp_math_RM_ADDRESS0   0X00040
-#define rp_math_RM_CONTROL0   0X00044
+#define vsm_off | rm_math_RM_ADDRESS0   0X00040
+#define vsm_off | rm_math_RM_CONTROL0   0X00044
 
-#define rp_math_BS_ID0        0X00060
-#define rp_math_BS_ADDRESS0   0X00064
-#define rp_math_BS_SIZE0      0X00068
+#define vsm_off | rm_math_BS_ID0        0X00060
+#define vsm_off | rm_math_BS_ADDRESS0   0X00064
+#define vsm_off | rm_math_BS_SIZE0      0X00068
 
 void Xil_Out32(unsigned int *icap, unsigned int off, unsigned int val) { 
     icap[off >> 2] = val;
@@ -54,6 +54,112 @@ void Xil_Out32(unsigned int *icap, unsigned int off, unsigned int val) {
 unsigned int Xil_In32(unsigned int *icap, unsigned int off) {
     return icap[off >> 2];
 }
+
+
+
+int do_pr(int pr_size, unsigned char *pr_data, unsigned int vsm) {
+    unsigned int vsm_off = vsm << 7;
+    int pr_ddr_addr = ddr_malloc(PR_MAX_SIZE);
+    if (pr_ddr_addr < 0) {
+        printf("apploadpr: ddr_malloc error a\n");
+        return 0;
+    }
+    printf("PR DDR address:0x%X\n", pr_ddr_addr);
+    printf("Begin to open devmem.\n");
+    int devmemfd = open(DEVMEM, O_RDWR | O_SYNC);
+    if (devmemfd < 1) {
+        printf("apploadpr: devmem open failed.\n");
+        return 0;
+    }
+    printf("Begin to map icap space.\n");
+    unsigned int *icap          = mmap(NULL, PRC_ICAP_SPAN, PROT_READ | PROT_WRITE, MAP_SHARED, devmemfd, XPAR_PRC_0_BASEADDR);
+    printf("Begin to map pr_ddr_pt space.\n");
+    unsigned int *pr_ddr_pt    = mmap(NULL, PR_MAX_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, devmemfd, pr_ddr_addr);
+    memcpy(pr_ddr_pt, pr_data, pr_size);
+    printf("Copy Sub.\n");
+
+    unsigned int Status;
+    unsigned int loading_done=0;
+
+    printf("Putting the PRC core's Math RP in Shutdown mode\n");
+    Xil_Out32(icap,vsm_off | rm_math_CONTROL,0);
+    printf("Waiting for the shutdown to occur\n");
+    while(!(Xil_In32(icap,vsm_off | rm_math_STATUS)&0x80));
+    printf("Math RP is shutdown\n");
+
+    printf("Initializing RM bitstream address and size registers for Math and Shift RMs\n");
+    Xil_Out32(icap,vsm_off | rm_math_BS_ADDRESS0,  pr_ddr_addr);
+    Xil_Out32(icap,vsm_off | rm_math_BS_SIZE0,   pr_size);
+
+    printf("Initializing RM trigger ID registers for Math and Shift RMs\n");
+    Xil_Out32(icap,vsm_off | rm_math_TRIGGER0,0);
+
+    printf("Initializing RM address and control registers for Math and Shift RMs\n");
+    Xil_Out32(icap,vsm_off | rm_math_RM_ADDRESS0,0);
+    Xil_Out32(icap,vsm_off | rm_math_RM_CONTROL0,0x1FF0);
+
+    printf("Reading RM bitstreams address and size registers for Math and Shift RMs\n");
+    printf("Adder RM address  = %x\n",Xil_In32(icap,vsm_off | rm_math_BS_ADDRESS0));
+    printf("Adder RM size     = %d Bytes\n",Xil_In32(icap,vsm_off | rm_math_BS_SIZE0));
+
+    printf("Reading RM Trigger and address registers for Math and Shift RMs\n");
+    printf("Adder RM Trigger0 = %x\n",Xil_In32(icap,vsm_off | rm_math_TRIGGER0));
+    printf("Adder RM Address0 = %x\n",Xil_In32(icap,vsm_off | rm_math_RM_ADDRESS0));
+
+    printf("Putting the PRC core's Math RP in Restart with Status mode\n");
+    Xil_Out32(icap,vsm_off | rm_math_CONTROL,2);
+    printf("Reading the Math RP status=%x\n",Xil_In32(icap,vsm_off | rm_math_STATUS));
+
+    printf("\n");
+
+    printf("Generating software trigger for VADD reconfiguration\n");
+    Status=Xil_In32(icap,vsm_off | rm_math_SW_TRIGGER);
+    if(!(Status&0x8000)) {
+      printf("Starting VADD Reconfiguration\n");
+      Xil_Out32(icap,vsm_off | rm_math_SW_TRIGGER,0);
+    }
+    loading_done = 0;
+    while(!loading_done) {
+        // printf("0x%x\n", Xil_In32(icap,vsm_off | rm_math_STATUS)&0x78);
+        Status=Xil_In32(icap,vsm_off | rm_math_STATUS)&0x07;
+        switch(Status) {
+            case 7 : 
+                printf("RM loaded\n"); 
+                loading_done=1; 
+                break;
+            case 6 : 
+                printf("RM is being reset\n"); 
+                break;
+            case 5 : 
+                printf("Software start-up step\n"); 
+                break;
+            case 4 : 
+                printf("."); 
+                break;
+            case 2 : 
+                printf("Software shutdown\n"); 
+                break;
+            case 1 : 
+                printf("Hardware shutdown\n"); 
+                break;
+            default:
+                break;
+        }
+    }
+    int ret = ddr_free(pr_ddr_addr);
+    if (ret < 0) {
+        printf("apploadpr: ddr_free error: pr_ddr_addr.\n");
+        return 0;
+    }
+    printf("\nVADD Reconfiguration Completed!\n");
+
+pr_exit:
+    munmap(icap, PRC_ICAP_SPAN);
+    munmap(pr_ddr_pt, PR_MAX_SIZE);
+    close(devmemfd);
+    return 1;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -109,109 +215,7 @@ int main(int argc, char *argv[])
 #endif
     */
 
-    int pr_ddr_addr = ddr_malloc(PR_MAX_SIZE);
-    if (pr_ddr_addr < 0) {
-        printf("apploadpr: ddr_malloc error a\n");
-        return 0;
-    }
-    printf("PR DDR address:0x%X\n", pr_ddr_addr);
-    printf("Begin to open devmem.\n");
-    int devmemfd = open(DEVMEM, O_RDWR | O_SYNC);
-    if (devmemfd < 1) {
-        printf("apploadpr: devmem open failed.\n");
-        return -1;
-    }
-    printf("Begin to map icap space.\n");
-    unsigned int *icap          = mmap(NULL, PRC_ICAP_SPAN, PROT_READ | PROT_WRITE, MAP_SHARED, devmemfd, XPAR_PRC_0_BASEADDR);
-    printf("Begin to map pr_ddr_pt space.\n");
-    unsigned int *pr_ddr_pt    = mmap(NULL, PR_MAX_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, devmemfd, pr_ddr_addr);
-    if (strcmp(argv[1], "add") == 0) {
-        memcpy(pr_ddr_pt, __vector_add_pr_add_bin, PR_SIZE);
-        printf("Copy Add.\n");
-    } else {
-        memcpy(pr_ddr_pt, __vector_sub_pr_sub_bin, PR_SIZE);
-        printf("Copy Sub.\n");
-    }
 
-    unsigned int Status;
-    unsigned int loading_done=0;
-
-    printf("Putting the PRC core's Math RP in Shutdown mode\n");
-    Xil_Out32(icap,rp_math_CONTROL,0);
-    printf("Waiting for the shutdown to occur\n");
-    while(!(Xil_In32(icap,rp_math_STATUS)&0x80));
-    printf("Math RP is shutdown\n");
-
-    printf("Initializing RM bitstream address and size registers for Math and Shift RMs\n");
-    Xil_Out32(icap,rp_math_BS_ADDRESS0,  pr_ddr_addr);
-    Xil_Out32(icap,rp_math_BS_SIZE0,   PR_SIZE);
-
-    printf("Initializing RM trigger ID registers for Math and Shift RMs\n");
-    Xil_Out32(icap,rp_math_TRIGGER0,0);
-
-    printf("Initializing RM address and control registers for Math and Shift RMs\n");
-    Xil_Out32(icap,rp_math_RM_ADDRESS0,0);
-    Xil_Out32(icap,rp_math_RM_CONTROL0,0x1FF0);
-
-    printf("Reading RM bitstreams address and size registers for Math and Shift RMs\n");
-    printf("Adder RM address  = %x\n",Xil_In32(icap,rp_math_BS_ADDRESS0));
-    printf("Adder RM size     = %d Bytes\n",Xil_In32(icap,rp_math_BS_SIZE0));
-
-    printf("Reading RM Trigger and address registers for Math and Shift RMs\n");
-    printf("Adder RM Trigger0 = %x\n",Xil_In32(icap,rp_math_TRIGGER0));
-    printf("Adder RM Address0 = %x\n",Xil_In32(icap,rp_math_RM_ADDRESS0));
-
-    printf("Putting the PRC core's Math RP in Restart with Status mode\n");
-    Xil_Out32(icap,rp_math_CONTROL,2);
-    printf("Reading the Math RP status=%x\n",Xil_In32(icap,rp_math_STATUS));
-
-    printf("\n");
-
-    printf("Generating software trigger for VADD reconfiguration\n");
-    Status=Xil_In32(icap,rp_math_SW_TRIGGER);
-    if(!(Status&0x8000)) {
-      printf("Starting VADD Reconfiguration\n");
-      Xil_Out32(icap,rp_math_SW_TRIGGER,0);
-    }
-    loading_done = 0;
-    while(!loading_done) {
-        // printf("0x%x\n", Xil_In32(icap,rp_math_STATUS)&0x78);
-        Status=Xil_In32(icap,rp_math_STATUS)&0x07;
-        switch(Status) {
-            case 7 : 
-                printf("RM loaded\n"); 
-                loading_done=1; 
-                break;
-            case 6 : 
-                printf("RM is being reset\n"); 
-                break;
-            case 5 : 
-                printf("Software start-up step\n"); 
-                break;
-            case 4 : 
-                printf("."); 
-                break;
-            case 2 : 
-                printf("Software shutdown\n"); 
-                break;
-            case 1 : 
-                printf("Hardware shutdown\n"); 
-                break;
-            default:
-                break;
-        }
-    }
-    int ret = ddr_free(pr_ddr_addr);
-    if (ret < 0) {
-        printf("apploadpr: ddr_free error: pr_ddr_addr.\n");
-        return 0;
-    }
-    printf("\nVADD Reconfiguration Completed!\n");
-
-pr_exit:
-    munmap(icap, PRC_ICAP_SPAN);
-    munmap(pr_ddr_pt, PR_MAX_SIZE);
-    close(devmemfd);
 
     return 0;
 }
