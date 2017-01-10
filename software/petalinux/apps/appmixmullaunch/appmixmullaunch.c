481536e1d3947ad4f4b86fe0e418058a9170fc34
Hongyuan Ding <hding4@gmail.com>
2017-01-09 22:53:26 -0600

    update appmixmullaunch

--- software/petalinux/apps/appmixmullaunch/appmixmullaunch.c
+++ software/petalinux/apps/appmixmullaunch/appmixmullaunch.c
@@ -23,6 +23,9 @@
 #include "../../../generic/include/base_addr.h"
 #include "../../../generic/include/thread_struct.h"
 
+#define ELF_LOAD_ADDR   ARM_DDR_BASE
+#define ELF_START_ADDR  SLAVE_INST_MEM_BASE
+
 #define PR_ROOT_PATH    "/mnt/pr_files/"
 #define ELF_FILE_NAME   "/mnt/elf_apps/"
 
@@ -30,7 +33,8 @@
 #define DEVMEM          "/dev/mem"
 
 struct app_t {
-    char *app_name;
+    char *hpp_name;
+    char *spp_name;
     unsigned int app_index;
     unsigned char priority;
     unsigned int data_size;
@@ -52,7 +56,8 @@ void init_app_seq(struct app_t *app_seq) {
     int i;
     for (i = 0; i < NUM_APP; i++) {
         unsigned int index = i / ( NUM_APP / NUM_APP_TYPE);
-        app_seq[i].app_name = app_name[index];
+        app_seq[i].hpp_name = hpp_name[index];
+        app_seq[i].spp_name = spp_name[index];
         app_seq[i].app_index = index;
         app_seq[i].priority = i % 2;
         switch (index) {
@@ -97,7 +102,8 @@ void print_seq(unsigned int *sequence, struct app_t *app_seq) {
     int i;
     for (i = 0; i < NUM_APP; i++) {
         printf("%d:\n", i);
-        printf("  app[%2d].name:        %s\n", sequence[i], app_seq[sequence[i]].app_name);
+        printf("  app[%2d].hname:       %s\n", sequence[i], app_seq[sequence[i]].hpp_name);
+        printf("  app[%2d].sname:       %s\n", sequence[i], app_seq[sequence[i]].spp_name);
         printf("  app[%2d].priority:    %d\n", sequence[i], app_seq[sequence[i]].priority);
         printf("  app[%2d].data_size:   %d\n", sequence[i], app_seq[sequence[i]].data_size);
         printf("  app[%2d].buffer_size: %d\n", sequence[i], app_seq[sequence[i]].buffer_size);
@@ -163,26 +169,41 @@ int main(int argc, char *argv[])
     strcat(trace_file_name, sw_group);
     printf("%s\n", trace_file_name);
 
-    pr_info_t  pr_info[NUM_APP_TYPE];
+    pr_info_t   pr_info[NUM_APP_TYPE];
+    elf_info_t  elf_info[NUM_APP_TYPE];
     char pr_file_path[128];
+    char elf_file_path[128];
     for (i = 0; i < NUM_APP_TYPE; i++) {
         int ret;
         strcpy(pr_file_path, PR_ROOT_PATH);
         strcat(pr_file_path, argv[2]);
         strcat(pr_file_path, "/");
-        strcat(pr_file_path, app_name[i]);
+        strcat(pr_file_path, hpp_name[i]);
         printf("pr_file_path: %s\n", pr_file_path);
         ret = pr_loader(pr_file_path, &(pr_info[i]));
         if (ret < 0) {
-            printf("apptes: pr_loader error: %s\n", app_name[i]);
+            printf("apptes: pr_loader error: %s\n", hpp_name[i]);
             return 0;
         }
-        printf("  Addr: 0x%08X\n", pr_info[i].ddr_addr);
+        printf("  PR Addr: 0x%08X\n", pr_info[i].ddr_addr);
+
+        strcpy(elf_file_path, ELF_FILE_NAME);
+        strcat(pr_file_path, spp_name[i]);
+        strcat(elf_file_path, ".elf");
+        printf("elf_file_path: %s\n", elf_file_path);
+        ret = elf_loader(elf_file_path, ELF_START_ADDR, &(elf_info[i]));
+        if (ret < 0) {
+            printf("apptest: elf_loader error\n");
+            return 0;
+        }
+        printf("  ELF size: 0x%08X\n", elf_info[i].thread_size);
+        printf("  ELF addr: 0x%08X\n", elf_info[i].ddr_addr);
+        printf("  ELF dmas: 0x%08X\n", elf_info[i].DMA_size);
     }
 
     for (i = 0; i < NUM_APP; i++) {
         unsigned int index = sequence[i];
-        char magic                  = app_seq[index].app_name[0];
+        char magic                  = app_seq[index].hpp_name[0];
         unsigned int app_index      = app_seq[index].app_index;
         unsigned char priority      = app_seq[index].priority;
         unsigned int data_size      = app_seq[index].data_size;
@@ -201,6 +222,11 @@ int main(int argc, char *argv[])
                 sp[i].group_num.id0         = 1;
                 sp[i].group_num.id1         = num_group;
                 sp[i].elf_info.elf_magic    = magic;
+                sp[i].elf_info.main_addr    = elf_info[0].main_addr;
+                sp[i].elf_info.stack_addr   = elf_info[0].stack_addr;
+                sp[i].elf_info.thread_size  = elf_info[0].thread_size;
+                sp[i].elf_info.ddr_addr     = elf_info[0].ddr_addr;
+                sp[i].elf_info.DMA_size     = elf_info[0].DMA_size;
                 sp[i].pr_info.ddr_addr      = pr_info[0].ddr_addr;
                 sp[i].pr_info.num_pr_file   = pr_info[0].num_pr_file;
                 sp[i].pr_info.each_size     = pr_info[0].each_size;
@@ -220,6 +246,11 @@ int main(int argc, char *argv[])
                 sp[i].group_num.id0         = 1;
                 sp[i].group_num.id1         = num_group;
                 sp[i].elf_info.elf_magic    = magic;
+                sp[i].elf_info.main_addr    = elf_info[1].main_addr;
+                sp[i].elf_info.stack_addr   = elf_info[1].stack_addr;
+                sp[i].elf_info.thread_size  = elf_info[1].thread_size;
+                sp[i].elf_info.ddr_addr     = elf_info[1].ddr_addr;
+                sp[i].elf_info.DMA_size     = elf_info[1].DMA_size;
                 sp[i].pr_info.ddr_addr      = pr_info[1].ddr_addr;
                 sp[i].pr_info.num_pr_file   = pr_info[1].num_pr_file;
                 sp[i].pr_info.each_size     = pr_info[1].each_size;
@@ -238,7 +269,12 @@ int main(int argc, char *argv[])
                 sp[i].group_size.id1        = data_size / buffer_size / num_group;
                 sp[i].group_num.id0         = num_group;
                 sp[i].group_num.id1         = num_group;
-                sp[i].elf_info.elf_magic    = magic; 
+                sp[i].elf_info.elf_magic    = magic;
+                sp[i].elf_info.main_addr    = elf_info[2].main_addr;
+                sp[i].elf_info.stack_addr   = elf_info[2].stack_addr;
+                sp[i].elf_info.thread_size  = elf_info[2].thread_size;
+                sp[i].elf_info.ddr_addr     = elf_info[2].ddr_addr;
+                sp[i].elf_info.DMA_size     = elf_info[2].DMA_size;
                 sp[i].pr_info.ddr_addr      = pr_info[2].ddr_addr;
                 sp[i].pr_info.num_pr_file   = pr_info[2].num_pr_file;
                 sp[i].pr_info.each_size     = pr_info[2].each_size;
@@ -256,6 +292,11 @@ int main(int argc, char *argv[])
                 sp[i].group_num.id0         = 1;
                 sp[i].group_num.id1         = num_group;
                 sp[i].elf_info.elf_magic    = magic;
+                sp[i].elf_info.main_addr    = elf_info[3].main_addr;
+                sp[i].elf_info.stack_addr   = elf_info[3].stack_addr;
+                sp[i].elf_info.thread_size  = elf_info[3].thread_size;
+                sp[i].elf_info.ddr_addr     = elf_info[3].ddr_addr;
+                sp[i].elf_info.DMA_size     = elf_info[3].DMA_size;
                 sp[i].pr_info.ddr_addr      = pr_info[3].ddr_addr;
                 sp[i].pr_info.num_pr_file   = pr_info[3].num_pr_file;
                 sp[i].pr_info.each_size     = pr_info[3].each_size;
@@ -265,7 +306,8 @@ int main(int argc, char *argv[])
                 printf("Magic number is wrong.\n");
                 break;
         }
-        printf("%s\n", app_seq[index].app_name); 
+        printf("%s\n", app_seq[index].hpp_name); 
+        printf("%s\n", app_seq[index].spp_name); 
         print_struct(&sp[i]);
     }
 
@@ -381,7 +423,14 @@ int main(int argc, char *argv[])
             return 0;
         }        
     }
-
+    // free elf
+    for (i = 0; i < NUM_APP_TYPE; i++) {
+        ret = ddr_free(elf_info[i].ddr_addr);
+        if (ret < 0) {
+            printf("elf ddr_free error: %d\n", i);
+            return 0;
+        }        
+    }
     return 0;
 }
 
